e5af4e724acf5b54e3d2e59c2974ec60b67359b2
Charles Huffman <huffmancharles14@gmail.com>
2024-07-15 11:06:14 -0500

    add basic css, display weather data

--- index.js
+++ index.js
@@ -3,21 +3,48 @@
 // We can get the data now we have currentConditions, and days for forecast.
 //we can edit the URL to get either metric or us standard measurements.
 // we can break down the url further to choose between US and Metric that way we don't have to do the math to convert temperatures.
+
+// Fetch data
 const UNIT_GROUP = "?unitGroup=us";
 const MY_API_KEY = "&key=XXGXHX3HNQFT2MBX2FVU7JYWQ";
 const URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
 
 const locationInput = document.querySelector('.location');
-const submit = document.querySelector('.submit')
+const submit = document.querySelector('.submit');
+const content = document.querySelector('.content-container');
+const weatherLocation = document.querySelector('.current-location');
+const currentConditionsContainer = document.querySelector('.current-conditions');
+const currentTemperature = document.querySelector('.temperature');
+const currentHumidity = document.querySelector('.humidity');
+const currentSunrise = document.querySelector('.sunrise');
+const currentSunset = document.querySelector('.sunset');
 
 async function fetchWeather(location) {
     const response = await fetch(`${URL}/${location}/${UNIT_GROUP}${MY_API_KEY}&contentType=json`, { mode: 'cors' });
-    if (response.ok) {
-        console.log(response.json())
+    if (!response.ok) {
+        console.log("You have an error with this fetch")
+    } else {
+        return response.json()
     }
+
 }
 
-submit.addEventListener('click', (e) => {
-    fetchWeather(locationInput.value)
+
+
+submit.addEventListener('click', async (e) => {
     e.preventDefault()
+    const weatherData = await fetchWeather(locationInput.value);
+    displayCity(weatherData);
 })
+
+function displayCity(data) {
+    const { temp, humidity, sunrise, sunset } = data.currentConditions
+    const city = data.resolvedAddress
+    weatherLocation.textContent = city
+    currentTemperature.textContent = `temperature ${temp}`
+    currentHumidity.textContent = `Humidity ${humidity}`
+    currentSunrise.textContent = `Sunrise ${sunrise}`
+    currentSunset.textContent = `Sunset ${sunset}`
+    console.log(data)
+}
+
